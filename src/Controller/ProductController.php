ada26c0d71642f20ac581850197925e8c6b8eceb
User <rachida.haddouch.webdev@gmail.com>
2021-03-24 11:32:46 +0100

    adding vue product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     /**
      * @Route("/products", name="products")
      */
     public function index(): Response
     {
-        return $this->render('product/index.html.twig');
+        $products = $this->entityManager->getRepository(Product::class)->findAll(); //chercher tous les produits
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
     }
+
+
+    /**
+     * @Route("/product/{slug}", name="product")
+     */
+    public function show($slug)
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        if (!$product){
+            return $this->redirectToRoute('products');
+
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+
+
+
 }
