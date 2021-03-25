8dda5ad537d9968fb929e4cde820e07128e1de1c
User <rachida.haddouch.webdev@gmail.com>
2021-03-25 14:21:49 +0100

    Creation functionality addresses for users and vue twig

---
+++ src/Form/AddressType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',TextType::class, [
+                'label' => 'The name of your address? *',
+                'attr' => [
+                    'placeholder' => 'Name of your address'
+                ]
+            ])
+            ->add('firstname',TextType::class, [
+                'label' => 'Fist Name *',
+                'attr' => [
+                    'placeholder' => 'your first name'
+                ]
+            ])
+            ->add('lastname',TextType::class, [
+                'label' => 'Last Name *',
+                'attr' => [
+                    'placeholder' => 'your last name'
+                ]
+            ])
+            ->add('company',TextType::class, [
+                'label' => 'Company Name',
+                'attr' => [
+                    'placeholder' => 'your company name'
+                ]
+            ])
+            ->add('address',TextType::class, [
+                'label' => 'Address *',
+                'attr' => [
+                    'placeholder' => 'your address'
+                ]
+            ])
+            ->add('postal',TextType::class, [
+                'label' => 'Postal code *',
+                'attr' => [
+                    'placeholder' => 'your postal code'
+                ]
+            ])
+            ->add('city',TextType::class, [
+                'label' => 'City *',
+                'attr' => [
+                    'placeholder' => 'your city'
+                ]
+            ])
+            ->add('country',CountryType::class, [
+                'label' => 'Country *',
+                'attr' => [
+                    'placeholder' => 'your country'
+                ]
+            ])
+            ->add('phone',TelType::class, [
+                'label' => 'Phone number *',
+                'attr' => [
+                    'placeholder' => 'your phone number'
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'Validate',
+                'attr' => [
+                    'class' => ' btn btn-block btn-info mt-3'
+                ]
+            ]  )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
