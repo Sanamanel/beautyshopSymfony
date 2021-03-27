95400abe0501b06bc4366a83428e7500d8f124b6
User <rachida.haddouch.webdev@gmail.com>
2021-03-27 16:01:39 +0100

    order crud

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -49,7 +49,7 @@ class StripeController extends AbstractController
         $products_for_stripe[] = [
             'price_data' => [
                 'currency' => 'eur',
-                'unit_amount' => $order->getCarrierPrice() * 100,
+                'unit_amount' => $order->getCarrierPrice(),
                 'product_data' => [
                     'name' => $order->getCarrierName(),
                     'images' => [$YOUR_DOMAIN],
